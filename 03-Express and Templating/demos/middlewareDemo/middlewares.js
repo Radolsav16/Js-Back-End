a389fb8eda0ec22372b9546ac1e4bd3387276d3d
Radoslav <todorovradoslav6@gmail.com>
2025-01-14 19:49:17 +0200

    Middleware demo

---
+++ 03-Express and Templating/demos/middlewareDemo/middlewares.js
@@ -0,0 +1,27 @@
+const middlewares = [];
+
+function use(middleware){
+    middlewares.push(middleware);
+}
+
+
+function execute(req,res){
+    let index  = 0;
+
+    const next = () => {
+
+    if(index >= middlewares.length){
+        return;
+    }
+        middlewares[index++](req,res);
+    }
+
+    next();
+
+    
+}
+
+export const middlewarechain ={
+    use,
+    execute
+}
