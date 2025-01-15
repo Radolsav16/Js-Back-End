0a088c1d3700492ed73965584e4a879b99003ebb
Radoslav <todorovradoslav6@gmail.com>
2025-01-15 22:50:22 +0200

    Show static pages and set static files in use

--- Movie Magic/src/index.js
+++ Movie Magic/src/index.js
@@ -11,6 +11,8 @@ app.engine('.hbs',handlebars.engine)
 app.set('view engine','.hbs');
 app.set('views','./src/views')
 
+app.use('/static',express.static('src/static'));
+
 
 
 
@@ -19,6 +21,13 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 
+app.get('/about',(req,res) => {
+    res.render('about')
+})
+
+app.get('*',(req,res)=>{
+    res.render('error');
+})
 
 
 app.listen(3030,()=> console.log('App is running on port 3030'));
