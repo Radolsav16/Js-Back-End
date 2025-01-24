7cc71c51e4a72463831548f2bee6fda8554ba881
Radoslav <todorovradoslav6@gmail.com>
2025-01-24 21:51:14 +0200

    Make details page works with dynamic info from database

--- Movie Magic/src/controllers/home.js
+++ Movie Magic/src/controllers/home.js
@@ -2,5 +2,6 @@ import { databaseApi } from "../service/dataApi.js"
 
 export default async function Home(req,res){
     const data = await databaseApi.getMovies()
+    console.log(data);
     res.render('home',{movies:data})
 }    
